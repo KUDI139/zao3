811a4b6ee8ea29730b151920e8639b0f62d8d72c
Kudi139 <95287528+Kudi139@users.noreply.github.com>
2021-11-30 11:20:32 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*!
+ * 作者：1396740034 
+ * 来看一下我的源码有什么不对的地方
+ * 及时反馈！
+ */
+console.log("\n %c Made with love %c by AyagawaSeirin | qwq.best ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
